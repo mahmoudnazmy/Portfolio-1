de80d43dc80d8ec2059540157c4e5224d619e143
12youssef2 <103836605+12youssef2@users.noreply.github.com>
2022-08-15 16:48:58 +0200

    Add files via upload

---
+++ js/master.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.writing', {
+    strings: [
+        'Web Developer',
+        'Web Designer',
+    ],
+    typeSpeed: 120,
+    backSpeed: 80,
+    loop: true,
+    smartBackspace: true
+
+});
